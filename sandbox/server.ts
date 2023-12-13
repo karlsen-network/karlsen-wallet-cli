eefe519247131bd027c199b2b09e262e36db28cf
Lemois 1337 <lemois1337@protonmail.com>
2023-12-13 20:49:08 +0100

    Removed TypeScript compiler necessarity

--- sandbox/server.ts
+++
@@ -1,6 +0,0 @@
-import {GRPCServer} from './lib/grpc-server';
-
-let server = new GRPCServer({
-	appFolder:process.cwd()
-});
-server.start();
--- sandbox/wallet-api-test.ts
+++
@@ -1,8 +0,0 @@
-import {WalletApi, Api, Error} from './lib/wallet-api';
-
-let walletApi = new WalletApi({
-	appFolder:process.cwd()
-});
-walletApi.getBlock({blockHash:"xyz", includeBlockVerboseData:true}, (error:Error, result:Api.BlockRes|null|undefined)=>{
-	console.log("getBlock:error, result", error, result);
-})
